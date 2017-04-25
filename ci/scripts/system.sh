a2c5c4971ed5e664b30074b4c63c284aa227968e
Jatin Naik <jnaik@pivotal.io>
2017-04-25 17:25:39 +0100

    Corrected paths for CA_CERT and gateway key

--- ci/scripts/system.sh
+++ ci/scripts/system.sh
@@ -12,12 +12,12 @@ ssh-add bosh-backup-and-restore-meta/keys/github
 export GOPATH=$PWD
 export PATH=$PATH:$GOPATH/bin
 export BOSH_URL="https://genesis-bosh.backup-and-restore.cf-app.com:25555"
-export BOSH_CERT_PATH=bosh-backup-and-restore-meta/certs/genesis-bosh.backup-and-restore.cf-app.com.crt
+export BOSH_CERT_PATH=`pwd`/bosh-backup-and-restore-meta/certs/genesis-bosh.backup-and-restore.cf-app.com.crt
 export BOSH_CLIENT
 export BOSH_CLIENT_SECRET
 export BOSH_GATEWAY_USER=vcap
 export BOSH_GATEWAY_HOST=genesis-bosh.backup-and-restore.cf-app.com
-export BOSH_GATEWAY_KEY=bosh-backup-and-restore-meta/genesis-bosh/bosh.pem
+export BOSH_GATEWAY_KEY=`pwd`/bosh-backup-and-restore-meta/genesis-bosh/bosh.pem
 export POSTGRES_PASSWORD
 
 
