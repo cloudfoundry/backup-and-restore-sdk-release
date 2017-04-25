4bdb3041cf4cd5da8aab84474c73325b9ebaf487
Giuseppe Capizzi <gcapizzi@pivotal.io>
2017-04-25 11:21:10 +0100

    Added a task to deploy the release
    [#143759817]
    
    Signed-off-by: Jatin Naik <jnaik@pivotal.io>

---
+++ ci/scripts/generate-manifest-and-deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eu
+
+./bosh-backup-and-restore-meta/unlock-ci.sh
+export BOSH_CLIENT
+export BOSH_CLIENT_SECRET
+
+bosh-cli --non-interactive -e genesis-bosh.backup-and-restore.cf-app.com \
+  --ca-cert "./bosh-backup-and-restore-meta/certs/genesis-bosh.backup-and-restore.cf-app.com.crt" \
+  -d postgres-dev \
+  deploy database-backup-and-restore/ci/manifests/postgres-dev.yml \
+  -v database-backup-and-restore-release-version=$(cat release-tarball/version) \
+  -v database-backup-and-restore-release-url=$(cat release-tarball/url) \
+  -v postgres-password=${POSTGRES_PASSWORD}
