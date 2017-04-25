cae29f264efad58483a877dba48b77d242ccd644
Giuseppe Capizzi <gcapizzi@pivotal.io>
2017-04-25 11:26:39 +0100

    Corrected test manifest path in deploy env
    [#143759817]
    
    Signed-off-by: Jatin Naik <jnaik@pivotal.io>

--- ci/scripts/generate-manifest-and-deploy.sh
+++ ci/scripts/generate-manifest-and-deploy.sh
@@ -9,7 +9,7 @@ export BOSH_CLIENT_SECRET
 bosh-cli --non-interactive -e genesis-bosh.backup-and-restore.cf-app.com \
   --ca-cert "./bosh-backup-and-restore-meta/certs/genesis-bosh.backup-and-restore.cf-app.com.crt" \
   -d postgres-dev \
-  deploy database-backup-and-restore/ci/manifests/postgres-dev.yml \
+  deploy database-backup-and-restore-release/ci/manifests/postgres-dev.yml \
   -v database-backup-and-restore-release-version=$(cat release-tarball/version) \
   -v database-backup-and-restore-release-url=$(cat release-tarball/url) \
   -v postgres-password=${POSTGRES_PASSWORD}
