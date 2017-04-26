5a65aa593a846f84ccba92692ec81dc35b31c63d
Giuseppe Capizzi <gcapizzi@pivotal.io>
2017-04-26 16:06:28 +0100

    Renamed the release created to match earlier format
    [#144349897]
    
    Signed-off-by: Jatin Naik <jnaik@pivotal.io>

--- ci/scripts/create-release.sh
+++ ci/scripts/create-release.sh
@@ -6,5 +6,5 @@ set -x
 export VERSION=$(cat version/number)
 
 pushd database-backup-and-restore-release
-  bosh-cli create-release --version $VERSION --tarball=../database-backup-and-restore-release-build/database-backup-and-restore-release-$VERSION.tgz --force
+  bosh-cli create-release --version $VERSION --tarball=../database-backup-and-restore-release-build/database-backup-and-restore-$VERSION.tgz --force
 popd
