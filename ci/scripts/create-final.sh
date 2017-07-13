77dbd16d0352034c88d4a38a7e045e44dc29606d
Rosie Bloxsom <rbloxsom@pivotal.io>
2017-07-13 16:09:55 +0100

    Add release final job for concourse pipeline
    [#148816475]
    
    Signed-off-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

---
+++ ci/scripts/create-final.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ex
+
+VERSION=$(cat version/number)
+
+pushd backup-and-restore-sdk-release
+  bosh -n create release --version "$VERSION" --final --with-tarball
+
+  mv releases/backup-and-restore-sdk/backup-and-restore-sdk-"${VERSION}".tgz \
+    ../backup-and-restore-sdk-final-release-tarball
+
+  git add ./releases
+  git add ./.final_builds
+
+  git config --global user.name "Backup & Restore Concourse"
+  git config --global user.email "cf-lazarus@pivotal.io"
+
+  git commit -m "Add final release ${VERSION} [ci skip]"
+popd
+
+cp -R backup-and-restore-sdk-release/. backup-and-restore-sdk-final-release
