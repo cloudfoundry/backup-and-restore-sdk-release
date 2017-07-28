1f58ee0c43e9a199a55ff341d6f115bf312f20d0
Henry Stanley <hstanley@pivotal.io>
2017-07-28 11:29:36 +0100

    Fix yaml formatting in private.yml
    [#149288111]

--- ci/scripts/create-final.sh
+++ ci/scripts/create-final.sh
@@ -7,10 +7,10 @@ VERSION=$(cat version/number)
 pushd backup-and-restore-sdk-release
   echo "---
 blobstore:
-provider: s3
-options:
-  access_key_id: $AWS_ACCESS_KEY_ID
-  secret_access_key: $AWS_SECRET_ACCESS_KEY
+  provider: s3
+  options:
+    access_key_id: $AWS_ACCESS_KEY_ID
+    secret_access_key: $AWS_SECRET_ACCESS_KEY
 " > config/private.yml
 
   bosh -n finalize release ../release-tarball/backup-and-restore-sdk-*.tgz --version "$VERSION"
