24cf1a78ef43433d0880be7948b1c1ccb90f7deb
Giuseppe Capizzi <gcapizzi@pivotal.io>
2017-09-12 15:52:42 +0100

    Add a unit test for VersionSafeInteractor
    [#150739240]

--- src/github.com/cloudfoundry-incubator/database-backup-and-restore/database/interactor.go
+++ src/github.com/cloudfoundry-incubator/database-backup-and-restore/database/interactor.go
@@ -5,14 +5,17 @@ import (
 	"github.com/cloudfoundry-incubator/database-backup-and-restore/version"
 )
 
+//go:generate counterfeiter -o fakes/fake_interactor.go . Interactor
 type Interactor interface {
 	Action(artifactFilePath string) error
 }
 
+//go:generate counterfeiter -o fakes/fake_server_version_detector.go . ServerVersionDetector
 type ServerVersionDetector interface {
 	GetVersion(config.ConnectionConfig) (version.SemanticVersion, error)
 }
 
+//go:generate counterfeiter -o fakes/fake_dump_utility_version_detector.go . DumpUtilityVersionDetector
 type DumpUtilityVersionDetector interface {
 	GetVersion() (version.SemanticVersion, error)
 }
