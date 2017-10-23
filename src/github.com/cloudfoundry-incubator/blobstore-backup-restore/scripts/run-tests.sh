f6f244c0e28ab04e63e617caa1376ca99d76e833
Giuseppe Capizzi <gcapizzi@pivotal.io>
2017-10-23 11:59:48 +0100

    Add scripts to run tests by fetching creds from LastPass
    [#151607399]
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- src/github.com/cloudfoundry-incubator/blobstore-backup-restore/scripts/run-tests.sh
+++ src/github.com/cloudfoundry-incubator/blobstore-backup-restore/scripts/run-tests.sh
@@ -19,30 +19,7 @@
 lpass show Shared-PCF-Backup-and-Restore/concourse-secrets --notes > \
   secrets.yml
 
-export BOSH_ENVIRONMENT="https://lite-bosh.backup-and-restore.cf-app.com:25555"
-export BOSH_CA_CERT="~/workspace/bosh-backup-and-restore-meta/certs/lite-bosh.backup-and-restore.cf-app.com.crt"
-export BOSH_CLIENT="admin"
-export BOSH_CLIENT_SECRET=$(lpass show "LiteBoshDirector" --password)
-export BOSH_GW_USER=vcap
-export BOSH_GW_HOST=lite-bosh.backup-and-restore.cf-app.com
-export BOSH_GW_PRIVATE_KEY=~/workspace/bosh-backup-and-restore-meta/genesis-bosh/bosh.pem
-export POSTGRES_PASSWORD=postgres_password
-export MYSQL_PASSWORD=mysql_password
-export BOSH_DEPLOYMENT="s3-backuper"
-export AWS_TEST_BUCKET_NAME="bbr-system-test-bucket"
-export AWS_TEST_BUCKET_REGION="eu-west-1"
 export AWS_ACCESS_KEY_ID="$(bosh-cli int --path=/aws-access-key-id secrets.yml)"
 export AWS_SECRET_ACCESS_KEY="$(bosh-cli int --path=/aws-secret-access-key secrets.yml)"
 
-TEST_SUITE=""
-if [[ $# -ge 1 ]]; then
-    TEST_SUITE=$1
-fi
-
-pushd $(dirname $0)/..
-    if [[ "${TEST_SUITE}" == "" ]]; then
-        ginkgo system_tests -trace
-    else
-        ginkgo --focus=${TEST_SUITE} system_tests -trace
-    fi
-popd
+ginkgo
