bd3d0ad942daa7edf397a249885c4aa8ec532507
Jatin Naik <jnaik@pivotal.io>
2017-11-29 13:17:43 +0000

    added a script to generate the spec file for golang packages
    [#152431275]

---
+++ scripts/make_all_golang_project_spec_files.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eux
+
+pushd $(dirname $0)
+  ./make_golang_project_spec_file.sh blobstore-backup-restorer blobstore-backup-restore
+  ./make_golang_project_spec_file.sh database-backup-restorer database-backup-restore
+popd
