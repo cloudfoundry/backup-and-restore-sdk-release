48b4bffc3ccbcf945ad204def6feea7a59b5aff0
Mirah Gary <mgary@pivotal.io>
2018-03-01 12:13:58 +0000

    Implement serialized implementation of multi-part copy for large blobs and tests
    Handle copy between different s3 regions with seperate S3Api objects for source and target regions
    
    [#155587393]
    
    Signed-off-by: Ed Cook <ecook@pivotal.io>

--- src/github.com/cloudfoundry-incubator/blobstore-backup-restore/s3_api.go
+++ src/github.com/cloudfoundry-incubator/blobstore-backup-restore/s3_api.go
@@ -3,6 +3,8 @@ package blobstore
 import (
 	"fmt"
 
+	"math"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -41,7 +43,33 @@ func (s3Api S3Api) IsVersioned(bucketName string) (bool, error) {
 	return output != nil && output.Status != nil && *output.Status == "Enabled", nil
 }
 
-func (s3Api S3Api) CopyVersion(sourceBucketName, blobKey, versionId, destinationBucketName string) error {
+func (s3Api S3Api) GetBlobSize(bucketName, blobKey, versionId string) (int64, error) {
+	headObjectOutput, err := s3Api.s3Client.HeadObject(&s3.HeadObjectInput{
+		Bucket:    aws.String(bucketName),
+		Key:       aws.String(blobKey),
+		VersionId: aws.String(versionId),
+	})
+
+	if err != nil {
+		return 0, fmt.Errorf("failed to retrieve head object output: %s", err)
+	}
+
+	return *headObjectOutput.ContentLength, nil
+}
+
+func (s3Api S3Api) CopyVersion(sourceBucketName, blobKey, versionId, destinationBucketName string, blobSize int64) error {
+	if sizeInMbs(blobSize) <= 100 {
+		return s3Api.copyVersion(sourceBucketName, blobKey, versionId, destinationBucketName)
+	} else {
+		return s3Api.copyVersionWithMultipart(sourceBucketName, blobKey, versionId, destinationBucketName, blobSize)
+	}
+}
+
+func sizeInMbs(sizeInBytes int64) int64 {
+	return sizeInBytes / (1024 * 1024)
+}
+
+func (s3Api S3Api) copyVersion(sourceBucketName, blobKey, versionId, destinationBucketName string) error {
 	_, err := s3Api.s3Client.CopyObject(&s3.CopyObjectInput{
 		Bucket:     aws.String(destinationBucketName),
 		Key:        aws.String(blobKey),
@@ -49,6 +77,59 @@ func (s3Api S3Api) CopyVersion(sourceBucketName, blobKey, versionId, destination
 	})
 
 	return err
+}
+
+func (s3Api S3Api) copyVersionWithMultipart(sourceBucketName, blobKey, versionId, destinationBucketName string, blobSize int64) error {
+	createOutput, err := s3Api.s3Client.CreateMultipartUpload(&s3.CreateMultipartUploadInput{
+		Bucket: aws.String(destinationBucketName),
+		Key:    aws.String(blobKey),
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to create multipart upload: %s", err)
+	}
+
+	var partSize int64 = 10 * 1024 * 1024
+	var partNumber int64 = 1
+	var parts []*s3.CompletedPart
+	for i := int64(0); i < blobSize; i += partSize {
+		upperLimit := int64(math.Min(float64(i+partSize-1), float64(blobSize)-1))
+
+		copyPartOutput, err := s3Api.s3Client.UploadPartCopy(&s3.UploadPartCopyInput{
+			Bucket:          aws.String(destinationBucketName),
+			Key:             aws.String(blobKey),
+			UploadId:        createOutput.UploadId,
+			CopySource:      aws.String(fmt.Sprintf("/%s/%s?versionId=%s", sourceBucketName, blobKey, versionId)),
+			CopySourceRange: aws.String(fmt.Sprintf("bytes=%d-%d", i, upperLimit)),
+			PartNumber:      aws.Int64(partNumber),
+		})
+
+		if err != nil {
+			return fmt.Errorf("failed to upload part with range: %d-%d: %s", i, upperLimit, err)
+		}
+
+		parts = append(parts, &s3.CompletedPart{
+			PartNumber: aws.Int64(partNumber),
+			ETag:       copyPartOutput.CopyPartResult.ETag,
+		})
+
+		partNumber++
+	}
+
+	_, err = s3Api.s3Client.CompleteMultipartUpload(&s3.CompleteMultipartUploadInput{
+		Bucket:   aws.String(destinationBucketName),
+		Key:      aws.String(blobKey),
+		UploadId: createOutput.UploadId,
+		MultipartUpload: &s3.CompletedMultipartUpload{
+			Parts: parts,
+		},
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to complete multipart upload: %s", err)
+	}
+
+	return nil
 }
 
 func (s3Api S3Api) DeleteObject(bucketName, file string) error {
