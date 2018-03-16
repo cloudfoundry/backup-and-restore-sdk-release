9a3db02266c204f0674343325b2a6bc45da32c10
Giuseppe Capizzi <gcapizzi@pivotal.io>
2018-03-16 14:49:59 +0000

    Test for 'no such host' errors
    These errors are caused by the usage of the virtual-host style to
    address buckets instead of the path-style.
    
    [#150253452]

--- src/github.com/cloudfoundry-incubator/blobstore-backup-restore/system_tests/s3_blobstore_system_test.go
+++ src/github.com/cloudfoundry-incubator/blobstore-backup-restore/system_tests/s3_blobstore_system_test.go
@@ -139,6 +139,7 @@ var _ = Describe("S3 backuper", func() {
 
 		Expect(session).To(gexec.Exit(1))
 		Expect(session.Out).NotTo(gbytes.Say("CERTIFICATE_VERIFY_FAILED"))
+		Expect(session.Out).NotTo(gbytes.Say("no such host"))
 	})
 })
 
