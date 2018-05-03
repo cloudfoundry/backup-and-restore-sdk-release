9dbf1cd661d5e4ce6277c39ef2f657e0e3e7935d
Giuseppe Capizzi <gcapizzi@pivotal.io>
2018-05-03 11:04:30 +0100

    Delete FlakeAttemps for Azure blobstore unit test
    [#156302315]
    
    Signed-off-by: Chunyi Lyu <clyu@pivotal.io>

--- ci/scripts/blobstore-unit-tests.sh
+++ ci/scripts/blobstore-unit-tests.sh
@@ -39,5 +39,5 @@ pushd backup-and-restore-sdk-release/src/github.com/cloudfoundry-incubator/s3-bl
 popd
 
 pushd backup-and-restore-sdk-release/src/github.com/cloudfoundry-incubator/azure-blobstore-backup-restore
-  ginkgo -r -p -v -skipPackage=system_tests -keepGoing --flakeAttempts=2
+  ginkgo -r -p -v -skipPackage=system_tests -keepGoing
 popd
