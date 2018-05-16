9e782bf6bad94dee18cede50a741145373ac91bf
Chunyi Lyu <clyu@pivotal.io>
2018-05-16 16:13:14 +0100

    Support overriding bosh configuration in s3-system-tests task
    [#157474975]
    
    Signed-off-by: Emmanouil Kiagias <ekiagias@pivotal.io>

--- ci/scripts/s3-system-tests.sh
+++ ci/scripts/s3-system-tests.sh
@@ -9,13 +9,13 @@ chmod 400 bosh-backup-and-restore-meta/keys/github
 ssh-add bosh-backup-and-restore-meta/keys/github
 chmod 400 bosh-backup-and-restore-meta/genesis-bosh/bosh.pem
 
-export GOPATH=$PWD/backup-and-restore-sdk-release
-export PATH=$PATH:$GOPATH/bin
-export BOSH_ENVIRONMENT="https://lite-bosh.backup-and-restore.cf-app.com"
-export BOSH_CA_CERT=`pwd`/bosh-backup-and-restore-meta/certs/lite-bosh.backup-and-restore.cf-app.com.crt
-export BOSH_GW_USER=vcap
-export BOSH_GW_HOST=lite-bosh.backup-and-restore.cf-app.com
-export BOSH_GW_PRIVATE_KEY=`pwd`/bosh-backup-and-restore-meta/genesis-bosh/bosh.pem
+export GOPATH="${PWD}/backup-and-restore-sdk-release"
+export PATH="$PATH:$GOPATH/bin"
+export BOSH_ENVIRONMENT="${BOSH_ENVIRONMENT:="https://lite-bosh.backup-and-restore.cf-app.com"}"
+export BOSH_CA_CERT="${BOSH_CA_CERT:="${PWD}/bosh-backup-and-restore-meta/certs/lite-bosh.backup-and-restore.cf-app.com.crt"}"
+export BOSH_GW_USER="${BOSH_GW_USER:="vcap"}"
+export BOSH_GW_HOST="${BOSH_GW_HOST:="lite-bosh.backup-and-restore.cf-app.com"}"
+export BOSH_GW_PRIVATE_KEY="${BOSH_GW_PRIVATE_KEY:="${PWD}/bosh-backup-and-restore-meta/genesis-bosh/bosh.pem"}"
 
 cd backup-and-restore-sdk-release/src/github.com/cloudfoundry-incubator/s3-blobstore-backup-restore
 
