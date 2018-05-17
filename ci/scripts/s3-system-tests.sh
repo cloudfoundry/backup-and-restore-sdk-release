417f5957274745aa7e9c413fd309543e69815bb8
Chunyi Lyu <clyu@pivotal.io>
2018-05-17 11:23:05 +0100

    Save bosh ssh key to file
    [#157474975]
    
    Signed-off-by: Mirah Gary <mgary@pivotal.io>

--- ci/scripts/s3-system-tests.sh
+++ ci/scripts/s3-system-tests.sh
@@ -17,6 +17,14 @@ export BOSH_GW_USER="${BOSH_GW_USER:="vcap"}"
 export BOSH_GW_HOST="${BOSH_GW_HOST:="lite-bosh.backup-and-restore.cf-app.com"}"
 export BOSH_GW_PRIVATE_KEY="${BOSH_GW_PRIVATE_KEY:="${PWD}/bosh-backup-and-restore-meta/genesis-bosh/bosh.pem"}"
 
+if [[ -z "${BOSH_GW_PRIVATE_KEY}" ]]; then
+  BOSH_GW_PRIVATE_KEY="${PWD}/bosh-backup-and-restore-meta/genesis-bosh/bosh.pem"
+else
+  echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
+  chmod 0600 "${PWD}/ssh.key"
+  BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
+fi
+
 cd backup-and-restore-sdk-release/src/github.com/cloudfoundry-incubator/s3-blobstore-backup-restore
 
 if [[ ! -z "${SKIP_TESTS}" ]]; then
