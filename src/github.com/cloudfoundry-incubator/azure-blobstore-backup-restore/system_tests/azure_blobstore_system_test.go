e2c3ef4707633c2f6904eab22c8a5ed8f548b4d0
Giuseppe Capizzi <gcapizzi@pivotal.io>
2018-06-01 14:12:16 +0100

    Add azure different storage account to system tests
    To test restore into a different storage account
    
    [#157994374]
    
    Signed-off-by: Josh Hill <jhill@pivotal.io>

--- src/github.com/cloudfoundry-incubator/azure-blobstore-backup-restore/system_tests/azure_blobstore_system_test.go
+++ src/github.com/cloudfoundry-incubator/azure-blobstore-backup-restore/system_tests/azure_blobstore_system_test.go
@@ -68,15 +68,15 @@ var _ = Describe("Azure backup and restore", func() {
 
 	Context("when the destination container is different from the source container", func() {
 		var restoreInstance JobInstance
-		var cloneContainerName string
+		var differentContainerName string
 
 		BeforeEach(func() {
 			restoreInstance = JobInstance{
 				Deployment:    MustHaveEnv("BOSH_DEPLOYMENT"),
-				Instance:      "azure-restore-to-clone",
+				Instance:      "azure-restore-to-different-container",
 				InstanceIndex: "0",
 			}
-			cloneContainerName = MustHaveEnv("AZURE_CLONE_CONTAINER_NAME")
+			differentContainerName = MustHaveEnv("AZURE_DIFFERENT_CONTAINER_NAME")
 			restoreInstance.RunOnInstanceAndSucceed("mkdir -p " + instanceArtifactDirPath)
 		})
 
@@ -85,9 +85,9 @@ var _ = Describe("Azure backup and restore", func() {
 			err := os.RemoveAll(localArtifactDirectory)
 			Expect(err).NotTo(HaveOccurred())
 
-			DeleteFileInContainer(cloneContainerName, fileName1)
-			DeleteFileInContainer(cloneContainerName, fileName2)
-			DeleteFileInContainer(cloneContainerName, fileName3)
+			DeleteFileInContainer(differentContainerName, fileName1)
+			DeleteFileInContainer(differentContainerName, fileName2)
+			DeleteFileInContainer(differentContainerName, fileName3)
 		})
 
 		It("backs up and restores cloned container successfully", func() {
@@ -105,9 +105,54 @@ var _ = Describe("Azure backup and restore", func() {
 
 			restoreInstance.RunOnInstanceAndSucceed("BBR_ARTIFACT_DIRECTORY=" + instanceArtifactDirPath + " /var/vcap/jobs/azure-blobstore-backup-restorer/bin/bbr/restore")
 
-			Expect(ReadFileFromContainer(cloneContainerName, fileName1)).To(Equal("TEST_BLOB_1"))
-			Expect(ReadFileFromContainer(cloneContainerName, fileName2)).To(Equal("TEST_BLOB_2"))
-			Expect(ReadFileFromContainer(cloneContainerName, fileName3)).To(Equal("TEST_BLOB_3"))
+			Expect(ReadFileFromContainer(differentContainerName, fileName1)).To(Equal("TEST_BLOB_1"))
+			Expect(ReadFileFromContainer(differentContainerName, fileName2)).To(Equal("TEST_BLOB_2"))
+			Expect(ReadFileFromContainer(differentContainerName, fileName3)).To(Equal("TEST_BLOB_3"))
+		})
+	})
+
+	Context("when the destination storage account is different from the source storage account", func() {
+		var restoreInstance JobInstance
+		var differentContainerName string
+
+		BeforeEach(func() {
+			restoreInstance = JobInstance{
+				Deployment:    MustHaveEnv("BOSH_DEPLOYMENT"),
+				Instance:      "azure-restore-to-different-storage-account",
+				InstanceIndex: "0",
+			}
+			differentContainerName = MustHaveEnv("AZURE_DIFFERENT_CONTAINER_NAME")
+			restoreInstance.RunOnInstanceAndSucceed("mkdir -p " + instanceArtifactDirPath)
+		})
+
+		AfterEach(func() {
+			restoreInstance.RunOnInstanceAndSucceed("sudo rm -rf " + instanceArtifactDirPath)
+			err := os.RemoveAll(localArtifactDirectory)
+			Expect(err).NotTo(HaveOccurred())
+
+			DeleteFileInContainer(differentContainerName, fileName1)
+			DeleteFileInContainer(differentContainerName, fileName2)
+			DeleteFileInContainer(differentContainerName, fileName3)
+		})
+
+		It("backs up and restores cloned container successfully", func() {
+			WriteFileInContainer(containerName, fileName1, "TEST_BLOB_1")
+			WriteFileInContainer(containerName, fileName2, "TEST_BLOB_2")
+			WriteFileInContainer(containerName, fileName3, "TEST_BLOB_3")
+
+			instance.RunOnInstanceAndSucceed("BBR_ARTIFACT_DIRECTORY=" + instanceArtifactDirPath + " /var/vcap/jobs/azure-blobstore-backup-restorer/bin/bbr/backup")
+
+			WriteFileInContainer(containerName, fileName2, "TEST_BLOB_2_NEW")
+			DeleteFileInContainer(containerName, fileName3)
+
+			instance.DownloadFromInstance(instanceArtifactDirPath+"/blobstore.json", localArtifactDirectory)
+			restoreInstance.UploadToInstance(localArtifactDirectory+"/blobstore.json", instanceArtifactDirPath)
+
+			restoreInstance.RunOnInstanceAndSucceed("BBR_ARTIFACT_DIRECTORY=" + instanceArtifactDirPath + " /var/vcap/jobs/azure-blobstore-backup-restorer/bin/bbr/restore")
+
+			Expect(ReadFileFromContainer(differentContainerName, fileName1)).To(Equal("TEST_BLOB_1"))
+			Expect(ReadFileFromContainer(differentContainerName, fileName2)).To(Equal("TEST_BLOB_2"))
+			Expect(ReadFileFromContainer(differentContainerName, fileName3)).To(Equal("TEST_BLOB_3"))
 		})
 	})
 })
