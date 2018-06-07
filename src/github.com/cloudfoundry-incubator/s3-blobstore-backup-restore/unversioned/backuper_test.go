eb5108401c28ad288260d203f8ad31e100941994
Giuseppe Capizzi <gcapizzi@pivotal.io>
2018-06-07 11:20:21 +0100

    Test Backuper with multiple BucketPairs
    [#158066234]

--- src/github.com/cloudfoundry-incubator/s3-blobstore-backup-restore/unversioned/backuper_test.go
+++ src/github.com/cloudfoundry-incubator/s3-blobstore-backup-restore/unversioned/backuper_test.go
@@ -12,28 +12,43 @@ import (
 var _ = Describe("Backuper", func() {
 
 	var (
-		dropletBucketPair   *fakes.FakeBucketPair
-		artifact            *fakes.FakeArtifact
-		fakeClock           *fakes.FakeClock
-		backuper            unversioned.Backuper
-		backupBucketAddress unversioned.BackupBucketAddress
-		err                 error
+		dropletsBucketPair            *fakes.FakeBucketPair
+		buildpacksBucketPair          *fakes.FakeBucketPair
+		artifact                      *fakes.FakeArtifact
+		fakeClock                     *fakes.FakeClock
+		backuper                      unversioned.Backuper
+		dropletsBackupBucketAddress   unversioned.BackupBucketAddress
+		buildpacksBackupBucketAddress unversioned.BackupBucketAddress
+		err                           error
 	)
 
 	BeforeEach(func() {
-		dropletBucketPair = new(fakes.FakeBucketPair)
-		backupBucketAddress = unversioned.BackupBucketAddress{
-			BucketName:   "the-backup-bucket",
-			BucketRegion: "the-backup-region",
+		dropletsBucketPair = new(fakes.FakeBucketPair)
+		dropletsBackupBucketAddress = unversioned.BackupBucketAddress{
+			BucketName:   "the-droplets-backup-bucket",
+			BucketRegion: "the-droplets-backup-region",
 			Path:         "time-now-in-seconds/droplets",
 		}
-		dropletBucketPair.BackupReturns(backupBucketAddress, nil)
-		artifact = new(fakes.FakeArtifact)
-		bucketPairs := map[string]unversioned.BucketPair{
-			"droplets": dropletBucketPair,
+		dropletsBucketPair.BackupReturns(dropletsBackupBucketAddress, nil)
+
+		buildpacksBucketPair = new(fakes.FakeBucketPair)
+		buildpacksBackupBucketAddress = unversioned.BackupBucketAddress{
+			BucketName:   "the-buildpacks-backup-bucket",
+			BucketRegion: "the-buildpacks-backup-region",
+			Path:         "time-now-in-seconds/buildpacks",
 		}
+		buildpacksBucketPair.BackupReturns(buildpacksBackupBucketAddress, nil)
+
+		bucketPairs := map[string]unversioned.BucketPair{
+			"droplets":   dropletsBucketPair,
+			"buildpacks": buildpacksBucketPair,
+		}
+
+		artifact = new(fakes.FakeArtifact)
+
 		fakeClock = new(fakes.FakeClock)
 		fakeClock.NowReturns("time-now-in-seconds")
+
 		backuper = unversioned.NewBackuper(bucketPairs, artifact, fakeClock)
 	})
 
@@ -42,26 +57,30 @@ var _ = Describe("Backuper", func() {
 	})
 
 	It("copies from the live bucket to the backup bucket", func() {
-		Expect(dropletBucketPair.BackupCallCount()).To(Equal(1))
-		Expect(dropletBucketPair.BackupArgsForCall(0)).To(Equal("time-now-in-seconds/droplets"))
+		Expect(dropletsBucketPair.BackupCallCount()).To(Equal(1))
+		Expect(dropletsBucketPair.BackupArgsForCall(0)).To(Equal("time-now-in-seconds/droplets"))
+		Expect(buildpacksBucketPair.BackupCallCount()).To(Equal(1))
+		Expect(buildpacksBucketPair.BackupArgsForCall(0)).To(Equal("time-now-in-seconds/buildpacks"))
 	})
 
 	It("saves the artifact file", func() {
 		Expect(artifact.SaveCallCount()).To(Equal(1))
 		Expect(artifact.SaveArgsForCall(0)).To(Equal(map[string]unversioned.BackupBucketAddress{
-			"droplets": backupBucketAddress,
+			"droplets":   dropletsBackupBucketAddress,
+			"buildpacks": buildpacksBackupBucketAddress,
 		}))
 	})
 
-	Context("When the Backup call fails", func() {
+	Context("when any of the BucketPairs fails to backup", func() {
 		BeforeEach(func() {
-			dropletBucketPair.BackupReturns(unversioned.BackupBucketAddress{}, fmt.Errorf("BACKUP ERROR"))
+			buildpacksBucketPair.BackupReturns(unversioned.BackupBucketAddress{}, fmt.Errorf("BACKUP ERROR"))
 		})
 
 		It("exits gracefully", func() {
 			By("throwing an error", func() {
 				Expect(err).To(MatchError("BACKUP ERROR"))
 			})
+
 			By("not saving an artifact", func() {
 				Expect(artifact.SaveCallCount()).To(Equal(0))
 			})
@@ -72,6 +91,7 @@ var _ = Describe("Backuper", func() {
 		BeforeEach(func() {
 			artifact.SaveReturns(fmt.Errorf("SAVE ERROR"))
 		})
+
 		It("throws an error", func() {
 			Expect(err).To(MatchError("SAVE ERROR"))
 		})
