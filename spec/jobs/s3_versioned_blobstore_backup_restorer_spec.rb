783efd9a730d5d4394047a8c698de01bdd7bc46c
Glen Rodgers <grodgers@pivotal.io>
2018-11-30 15:35:59 +0000

    Check IAM and key pair are not both used in S3 versioned
    [#162048021]

--- spec/jobs/s3_versioned_blobstore_backup_restorer_spec.rb
+++ spec/jobs/s3_versioned_blobstore_backup_restorer_spec.rb
@@ -5,6 +5,7 @@ require 'bosh/template/test'
 describe 's3-versioned-blobstore-backup-restorer job' do
   let(:release) { Bosh::Template::Test::ReleaseDir.new(File.join(File.dirname(__FILE__), '../..')) }
   let(:job) { release.job('s3-versioned-blobstore-backup-restorer') }
+  let(:buckets_template) { job.template('config/buckets.json') }
   let(:backup_template) { job.template('bin/bbr/backup') }
   let(:restore_template) { job.template('bin/bbr/restore') }
 
@@ -29,6 +30,54 @@ describe 's3-versioned-blobstore-backup-restorer job' do
           config = backup_template.render("enabled" => true)
           expect(config).to include("backup")
           expect(config).not_to include("/var/vcap/jobs/bpm/bin/bpm run s3-versioned-blobstore-backup-restorer")
+        end
+      end
+
+      context 'and it is configured correctly' do
+        it 'succeeds' do
+          manifest = {
+            "enabled" => true,
+            "buckets" => {
+              "droplets"  => {
+                "name" => "the_droplets_bucket",
+                "region" => "eu-west-1",
+                "aws_access_key_id" => "AWS_ACCESS_KEY_ID",
+                "aws_secret_access_key" => "AWS_SECRET_ACCESS_KEY",
+                "endpoint" => "endpoint_to_s3_compatible_blobstore",
+                "use_iam_profile" => false,
+                "backup" => {
+                  "name" => "the_backup_droplets_bucket",
+                  "region" => "eu-west-2",
+                }
+              }
+            }
+          }
+          expect { backup_template.render(manifest) }.to_not(raise_error)
+          expect { buckets_template.render(manifest) }.to_not(raise_error)
+        end
+      end
+
+      context 'both secrets keys and an IAM profile are configured' do
+        it 'errors' do
+          manifest = {
+            "enabled" => true,
+            "buckets" => {
+              "droplets"  => {
+                "name" => "the_droplets_bucket",
+                "region" => "eu-west-1",
+                "aws_access_key_id" => "AWS_ACCESS_KEY_ID",
+                "aws_secret_access_key" => "AWS_SECRET_ACCESS_KEY",
+                "endpoint" => "endpoint_to_s3_compatible_blobstore",
+                "use_iam_profile" => true,
+                "backup" => {
+                  "name" => "the_droplets_backup_bucket",
+                  "region" => "eu-west-2",
+                }
+              },
+            }
+          }
+
+         expect { buckets_template.render(manifest) }.to(raise_error(RuntimeError, 'Invalid configuration, both the access key ID and the secret key pair and an IAM profile were used for bucket droplets'))
         end
       end
     end
