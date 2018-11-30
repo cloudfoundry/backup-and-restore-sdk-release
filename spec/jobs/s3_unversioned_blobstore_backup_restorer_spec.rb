13e801ce3671e9f547435aaa5deb91a9971802b5
Chunyi Lyu <clyu@pivotal.io>
2018-11-30 10:52:31 +0000

    Added templating validation for live and backup buckets for S3 unversioned
    [#162048021]
    
    Signed-off-by: Glen Rodgers <grodgers@pivotal.io>

--- spec/jobs/s3_unversioned_blobstore_backup_restorer_spec.rb
+++ spec/jobs/s3_unversioned_blobstore_backup_restorer_spec.rb
@@ -6,6 +6,7 @@ describe 's3-unversioned-blobstore-backup-restorer job' do
   let(:release) { Bosh::Template::Test::ReleaseDir.new(File.join(File.dirname(__FILE__), '../..')) }
   let(:job) { release.job('s3-unversioned-blobstore-backup-restorer') }
   let(:backup_template) { job.template('bin/bbr/backup') }
+  let(:buckets_template) { job.template('config/buckets.json') }
   let(:restore_template) { job.template('bin/bbr/restore') }
 
   describe 'backup' do
@@ -29,6 +30,89 @@ describe 's3-unversioned-blobstore-backup-restorer job' do
           config = backup_template.render("enabled" => true)
           expect(config).to include("backup")
           expect(config).not_to include("/var/vcap/jobs/bpm/bin/bpm run s3-unversioned-blobstore-backup-restorer")
+        end
+      end
+
+      context 'and it is configured correctly' do
+        it 'succeeds' do
+          manifest = {
+            "enabled" => true,
+            "buckets" => {
+              "droplets"  => {
+                "name" => "the_droplets_bucket",
+                "region" => "eu-west-1",
+                "aws_access_key_id" => "AWS_ACCESS_KEY_ID",
+                "aws_secret_access_key" => "AWS_SECRET_ACCESS_KEY",
+                "endpoint" => "endpoint_to_s3_compatible_blobstore",
+                "use_iam_profile" => false,
+                "backup" => {
+                  "name" => "the_backup_droplets_bucket",
+                  "region" => "eu-west-2",
+                }
+              }
+            }
+          }
+          expect { backup_template.render(manifest) }.to_not(raise_error)
+          expect { buckets_template.render(manifest) }.to_not(raise_error)
+        end
+      end
+
+      context 'and the backup bucket is the same as the live bucket' do
+        it 'errors' do
+          manifest = {
+            "enabled" => true,
+            "buckets" => {
+              "droplets"  => {
+                "name" => "the_droplets_bucket",
+                "region" => "eu-west-1",
+                "aws_access_key_id" => "AWS_ACCESS_KEY_ID",
+                "aws_secret_access_key" => "AWS_SECRET_ACCESS_KEY",
+                "endpoint" => "endpoint_to_s3_compatible_blobstore",
+                "use_iam_profile" => false,
+                "backup" => {
+                  "name" => "the_droplets_bucket",
+                  "region" => "eu-west-2",
+                }
+              }
+            }
+          }
+          expect { buckets_template.render(manifest) }.to(raise_error(RuntimeError, 'Invalid bucket configuration for droplets, name and backup.name must be distinct'))
+        end
+      end
+
+      context 'and the backup bucket is a different live bucket' do
+        it 'errors' do
+          manifest = {
+            "enabled" => true,
+            "buckets" => {
+              "droplets"  => {
+                "name" => "the_droplets_bucket",
+                "region" => "eu-west-1",
+                "aws_access_key_id" => "AWS_ACCESS_KEY_ID",
+                "aws_secret_access_key" => "AWS_SECRET_ACCESS_KEY",
+                "endpoint" => "endpoint_to_s3_compatible_blobstore",
+                "use_iam_profile" => false,
+                "backup" => {
+                  "name" => "my_packages_bucket",
+                  "region" => "eu-west-2",
+                }
+              },
+              "packages"  => {
+                "name" => "my_packages_bucket",
+                "region" => "eu-west-1",
+                "aws_access_key_id" => "AWS_ACCESS_KEY_ID",
+                "aws_secret_access_key" => "AWS_SECRET_ACCESS_KEY",
+                "endpoint" => "endpoint_to_s3_compatible_blobstore",
+                "use_iam_profile" => false,
+                "backup" => {
+                  "name" => "the_packages_bucket_backu",
+                  "region" => "eu-west-2",
+                }
+              }
+            }
+          }
+
+         expect { buckets_template.render(manifest) }.to(raise_error(RuntimeError, 'Invalid bucket configuration, my_packages_bucket is used as a source bucket and a backup bucket'))
         end
       end
     end
