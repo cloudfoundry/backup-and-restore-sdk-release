a17c040bf89ac4cba6f8782a27ce9944b6472059
Sarah Connor <sconnor@pivotal.io>
2019-02-06 16:25:39 +0000

    Add write logic and concept of Region to Backup Artifact
    [#162305294]
    
    Signed-off-by: Glen Rodgers <grodgers@pivotal.io>

--- src/github.com/cloudfoundry-incubator/s3-blobstore-backup-restore/incremental/artifact.go
+++ src/github.com/cloudfoundry-incubator/s3-blobstore-backup-restore/incremental/artifact.go
@@ -2,12 +2,14 @@ package incremental
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 )
 
 //go:generate counterfeiter -o fakes/fake_artifact.go . Artifact
 type Artifact interface {
 	Write(map[string]BucketBackup) error
+	Load() (map[string]BucketBackup, error)
 }
 
 type artifact struct {
@@ -22,6 +24,7 @@ func NewArtifact(path string) Artifact {
 
 type BucketBackup struct {
 	BucketName          string   `json:"bucket_name"`
+	BucketRegion        string   `json:"bucket_region"`
 	Blobs               []string `json:"blobs"`
 	BackupDirectoryPath string   `json:"backup_directory_path"`
 }
@@ -32,5 +35,25 @@ func (a artifact) Write(backups map[string]BucketBackup) error {
 		return err
 	}
 
-	return ioutil.WriteFile(a.path, filesContents, 0644)
+	err = ioutil.WriteFile(a.path, filesContents, 0644)
+	if err != nil {
+		return fmt.Errorf("could not write backup file: %s", err.Error())
+	}
+
+	return nil
+}
+
+func (a artifact) Load() (map[string]BucketBackup, error) {
+	bytes, err := ioutil.ReadFile(a.path)
+	if err != nil {
+		return nil, fmt.Errorf("could not read backup file: %s", err.Error())
+	}
+
+	var backup map[string]BucketBackup
+	err = json.Unmarshal(bytes, &backup)
+	if err != nil {
+		return nil, fmt.Errorf("backup file has an invalid format: %s", err.Error())
+	}
+
+	return backup, nil
 }
--- src/github.com/cloudfoundry-incubator/s3-blobstore-backup-restore/incremental/artifact_test.go
+++ src/github.com/cloudfoundry-incubator/s3-blobstore-backup-restore/incremental/artifact_test.go
@@ -12,26 +12,34 @@ import (
 
 var _ = Describe("Artifact", func() {
 	var artifactFile *os.File
+	var artifact incremental.Artifact
+
 	BeforeEach(func() {
 		var err error
 		artifactFile, err = ioutil.TempFile("", "s3_backup")
 		Expect(err).NotTo(HaveOccurred())
+	})
 
+	AfterEach(func() {
+		err := os.Remove(artifactFile.Name())
+		Expect(err).NotTo(HaveOccurred())
 	})
 
 	It("writes the backupArtifact", func() {
-		artifact := incremental.NewArtifact(artifactFile.Name())
+		artifact = incremental.NewArtifact(artifactFile.Name())
 
-		err := artifact.Write(map[string]incremental.BucketBackup{
+		backup := map[string]incremental.BucketBackup{
 			"bucket_id": {
-				BucketName: "backup-bucket",
+				BucketName:   "backup-bucket",
+				BucketRegion: "backup-bucket-region",
 				Blobs: []string{
 					"2000_01_02_03_04_05/bucket_id/f0/fd/blob1/uuid",
 					"2000_01_02_03_04_05/bucket_id/f0/fd/blob2/uuid",
 				},
 				BackupDirectoryPath: "2000_01_02_03_04_05/bucket_id",
 			},
-		})
+		}
+		err := artifact.Write(backup)
 
 		Expect(err).NotTo(HaveOccurred())
 
@@ -39,9 +47,51 @@ var _ = Describe("Artifact", func() {
 		Expect(fileContent).To(MatchJSON(`{
 			"bucket_id": {
 				"bucket_name": "backup-bucket",
+				"bucket_region": "backup-bucket-region",
 				"backup_directory_path": "2000_01_02_03_04_05/bucket_id",
 				"blobs": ["2000_01_02_03_04_05/bucket_id/f0/fd/blob1/uuid", "2000_01_02_03_04_05/bucket_id/f0/fd/blob2/uuid"]
 			}
 		}`))
+
+		savedBackup, err := artifact.Load()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(savedBackup).To(Equal(backup))
+	})
+
+	Context("when saving the file fails", func() {
+		BeforeEach(func() {
+			artifact = incremental.NewArtifact("/this/path/does/not/exist")
+		})
+
+		It("returns an error", func() {
+			err := artifact.Write(map[string]incremental.BucketBackup{})
+			Expect(err).To(MatchError(ContainSubstring("could not write backup file")))
+		})
+	})
+
+	Context("when reading the file fails", func() {
+		BeforeEach(func() {
+			artifact = incremental.NewArtifact("/this/path/does/not/exist")
+		})
+
+		It("returns an error", func() {
+			backup, err := artifact.Load()
+			Expect(backup).To(BeNil())
+			Expect(err).To(MatchError(ContainSubstring("could not read backup file")))
+		})
+	})
+
+	Context("when the artifact has an invalid format", func() {
+		BeforeEach(func() {
+			artifact = incremental.NewArtifact(artifactFile.Name())
+			err := ioutil.WriteFile(artifactFile.Name(), []byte("THIS IS NOT VALID JSON"), 0666)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("returns an error", func() {
+			backup, err := artifact.Load()
+			Expect(backup).To(BeNil())
+			Expect(err).To(MatchError(ContainSubstring("backup file has an invalid format")))
+		})
 	})
 })
