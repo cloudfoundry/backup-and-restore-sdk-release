23be1102c76cda662fbd5c258d1fc7283b2b2bf7
Josh Hill <jhill@pivotal.io>
2019-02-27 10:21:54 +0000

    Execute metadata script using bash
    [#164113389]
    
    Signed-off-by: Mehul Modha <mmodha@pivotal.io>

--- spec/jobs/s3_unversioned_blobstore_backup_restorer_spec.rb
+++ spec/jobs/s3_unversioned_blobstore_backup_restorer_spec.rb
@@ -210,45 +210,41 @@ describe 's3-unversioned-blobstore-backup-restorer job' do
 
   describe 'metadata' do
     let(:metadata_template) { job.template('bin/bbr/metadata') }
+    let(:metadata_script) { metadata_template.render({}) }
+    let(:metadata_cmd) { "bash -c '#{metadata_script}'" }
 
     it 'fails when called with an old version of bbr that does not export BBR_VERSION' do
-      backup_script = metadata_template.render({})
-      stdout_str, error_str, status = Open3.capture3(backup_script)
+      stdout_str, error_str, status = Open3.capture3(metadata_cmd)
       expect(status.success?).not_to eq(true)
       expect(error_str).to include('Error: BBR_VERSION is not set, please ensure you are using the latest version of bbr')
     end
 
     it 'fails to run when bbr major version set to string' do
-        backup_script = metadata_template.render({})
-        stdout_str, error_str, status = Open3.capture3({'BBR_VERSION' => 'foo.bar.baz'}, backup_script)
+        stdout_str, error_str, status = Open3.capture3({'BBR_VERSION' => 'foo.bar.baz'}, metadata_cmd)
         expect(status.success?).not_to eq(true)
         expect(stdout_str).to include('Error: BBR version must be a valid semVer')
     end
 
     it 'fails to run when bbr minor version set to string' do
-        backup_script = metadata_template.render({})
-        stdout_str, error_str, status = Open3.capture3({'BBR_VERSION' => '1.bar.baz'}, backup_script)
+        stdout_str, error_str, status = Open3.capture3({'BBR_VERSION' => '1.bar.baz'}, metadata_cmd)
         expect(status.success?).not_to eq(true)
         expect(stdout_str).to include('Error: BBR version must be a valid semVer')
     end
 
     it 'returns error if the bbr_version is less than 1.4.0' do
-      backup_script = metadata_template.render({})
-      stdout_str, error_str, status = Open3.capture3({'BBR_VERSION' => '1.3.2'}, backup_script)
+      stdout_str, error_str, status = Open3.capture3({'BBR_VERSION' => '1.3.2'}, metadata_cmd)
       expect(status.success?).not_to eq(true)
       expect(stdout_str).to include('Error: BBR version must be 1.4.0 or greater')
     end
 
     it 'succesfully runs if the bbr_version is 1.4.0' do
-        backup_script = metadata_template.render({})
-        stdout_str, error_str, status = Open3.capture3({'BBR_VERSION' => '1.4.0'}, backup_script)
+        stdout_str, error_str, status = Open3.capture3({'BBR_VERSION' => '1.4.0'}, metadata_cmd)
         expect(status.success?).to eq(true)
         expect(stdout_str).to include('---')
     end
 
     it 'succesfully runs if the bbr_version is greater than 1.4.0' do
-        backup_script = metadata_template.render({})
-        stdout_str, error_str, status = Open3.capture3({'BBR_VERSION' => '2.3.4'}, backup_script)
+        stdout_str, error_str, status = Open3.capture3({'BBR_VERSION' => '2.3.4'}, metadata_cmd)
         expect(status.success?).to eq(true)
         expect(stdout_str).to include('---')
     end
