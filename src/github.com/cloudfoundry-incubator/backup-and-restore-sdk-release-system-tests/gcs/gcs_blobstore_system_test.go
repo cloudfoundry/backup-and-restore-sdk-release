b0a0daabc00dcca375531d5f42ee5f059353a8f6
Mehul Modha <mmodha@pivotal.io>
2019-03-05 14:09:31 +0000

    Add system test for GCS skipping same buckets
    [#161570105]
    
    Signed-off-by: Emmanouil Kiagias <ekiagias@pivotal.io>

--- src/github.com/cloudfoundry-incubator/backup-and-restore-sdk-release-system-tests/gcs/gcs_blobstore_system_test.go
+++ src/github.com/cloudfoundry-incubator/backup-and-restore-sdk-release-system-tests/gcs/gcs_blobstore_system_test.go
@@ -58,6 +58,67 @@ var _ = Describe("GCS Blobstore System Tests", func() {
 				runTestWithBlobs(1)
 			})
 		})
+
+		Context("there are two live buckets pointing into the same bucket", func() {
+			BeforeEach(func() {
+				instance = JobInstance{
+					Deployment: MustHaveEnv("BOSH_DEPLOYMENT"),
+					Name:       "gcs-backuper-same-bucket",
+					Index:      "0",
+				}
+
+				instanceArtifactDirPath = "/var/vcap/store/gcs-blobstore-backup-restorer" + strconv.FormatInt(time.Now().Unix(), 10)
+				instance.RunSuccessfully("sudo mkdir -p " + instanceArtifactDirPath)
+
+				gcsClient.WriteBlobToBucket(bucket, "test_file", "TEST_BLOB")
+			})
+
+			It("creates a backup and restores", func() {
+				By("successfully running a backup", func() {
+					instance.RunSuccessfully("sudo BBR_ARTIFACT_DIRECTORY=" + instanceArtifactDirPath +
+						" /var/vcap/jobs/gcs-blobstore-backup-restorer/bin/bbr/backup")
+				})
+
+				By("creating a complete remote backup with only one bucket identifier", func() {
+					backupBucketFolders := gcsClient.ListDirsFromBucket(backupBucket)
+
+					Expect(backupBucketFolders).To(MatchRegexp(
+						".*\\d{4}_\\d{2}_\\d{2}_\\d{2}_\\d{2}_\\d{2}/"))
+
+					backupBucketIdentifiers := getContentsOfBackupBucket(gcsClient, backupBucketFolders, "")
+					Expect(backupBucketIdentifiers).To(ContainSubstring("bucket1"))
+					Expect(backupBucketIdentifiers).NotTo(ContainSubstring("bucket2"))
+
+					backupBucketContent := getContentsOfBackupBucket(gcsClient, backupBucketFolders, "bucket1")
+					Expect(backupBucketContent).To(ContainSubstring(fmt.Sprintf("test_file")))
+				})
+
+				By("generating a complete backup artifact", func() {
+					session := instance.Run(fmt.Sprintf("cat %s/%s", instanceArtifactDirPath, "blobstore.json"))
+					Expect(session).Should(gexec.Exit(0))
+					fileContents := string(session.Out.Contents())
+
+					Expect(fileContents).To(ContainSubstring("\"bucket1\":{"))
+					Expect(fileContents).To(ContainSubstring("\"bucket_name\":\"" + backupBucket + "\""))
+					Expect(fileContents).To(MatchRegexp(
+						"\"path\":\"\\d{4}_\\d{2}_\\d{2}_\\d{2}_\\d{2}_\\d{2}\\/bucket1\""))
+					Expect(fileContents).To(ContainSubstring("\"bucket2\":{"))
+					Expect(fileContents).To(ContainSubstring("\"same_bucket_as\":\"bucket1\""))
+					Expect(fileContents).To(ContainSubstring("\"path\":\"\""))
+					Expect(fileContents).To(ContainSubstring("\"bucket_name\":\"\""))
+				})
+
+				By("restoring from a backup artifact", func() {
+					gcsClient.DeleteBlobInBucket(bucket, "**/test_file")
+
+					instance.RunSuccessfully("sudo BBR_ARTIFACT_DIRECTORY=" + instanceArtifactDirPath +
+						" /var/vcap/jobs/gcs-blobstore-backup-restorer/bin/bbr/restore")
+
+					liveBucketContent := gcsClient.ListDirsFromBucket(bucket)
+					Expect(liveBucketContent).To(ContainSubstring("test_file"))
+				})
+			})
+		})
 	})
 })
 
