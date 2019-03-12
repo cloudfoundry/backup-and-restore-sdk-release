ba47920d778e2b2b65242e32637a25020624afda
Sarah Connor <sconnor@pivotal.io>
2019-03-12 11:58:18 +0000

    Localise lookup behaviour for MustHaveEnv
    - single use case for S3 endpoint
    
    [#163694344]
    
    Signed-off-by: Emmanouil Kiagias <ekiagias@pivotal.io>

--- src/github.com/cloudfoundry-incubator/s3-blobstore-backup-restore/s3bucket/s3_suite_test.go
+++ src/github.com/cloudfoundry-incubator/s3-blobstore-backup-restore/s3bucket/s3_suite_test.go
@@ -1,6 +1,8 @@
 package s3bucket_test
 
 import (
+	"os"
+
 	. "github.com/cloudfoundry-incubator/backup-and-restore-sdk-release-system-tests"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -9,9 +11,9 @@ import (
 )
 
 var (
+	S3Endpoint                   = MustHaveEnvOrBeEmpty("S3_ENDPOINT")
 	LiveRegion                   = MustHaveEnv("S3_LIVE_REGION")
 	BackupRegion                 = MustHaveEnv("S3_BACKUP_REGION")
-	S3Endpoint                   = MustHaveEnv("S3_ENDPOINT")
 	AccessKey                    = MustHaveEnv("S3_ACCESS_KEY_ID")
 	SecretKey                    = MustHaveEnv("S3_SECRET_ACCESS_KEY")
 	PreExistingBigFileBucketName = MustHaveEnv("S3_BIG_FILE_BUCKET")
@@ -21,4 +23,12 @@ var (
 func TestS3(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "S3 Suite")
+}
+
+func MustHaveEnvOrBeEmpty(keyname string) string {
+	val, exist := os.LookupEnv(keyname)
+	if !exist {
+		panic("Need " + keyname + " for the test")
+	}
+	return val
 }
