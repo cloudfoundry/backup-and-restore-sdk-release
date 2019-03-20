8d8bd3e8b93f4bf80f2e302e69a9085c2f6826bc
Emmanouil Kiagias <ekiagias@pivotal.io>
2019-03-20 12:00:12 +0000

    Removed explicit patches to make it clear what version we compare on
    [#162862299]
    
    Signed-off-by: Glen Rodgers <grodgers@pivotal.io>

--- src/github.com/cloudfoundry-incubator/database-backup-restore/database/interactor_factory.go
+++ src/github.com/cloudfoundry-incubator/database-backup-restore/database/interactor_factory.go
@@ -147,12 +147,12 @@ func (f InteractorFactory) getAdditionalOptionsProvider(mysqlVersion version.Dat
 
 func (f InteractorFactory) getUtilitiesForPostgres(postgresVersion version.DatabaseServerVersion) (string, string, string, error) {
 	semVer := postgresVersion.SemanticVersion
-	if semVer.MinorVersionMatches(version.SemVer("9", "4", "11")) {
+	if semVer.MinorVersionMatches(version.SemVer("9", "4", "x")) {
 		return f.utilitiesConfig.Postgres94.Client,
 			f.utilitiesConfig.Postgres94.Dump,
 			f.utilitiesConfig.Postgres94.Restore,
 			nil
-	} else if semVer.MinorVersionMatches(version.SemVer("9", "6", "3")) {
+	} else if semVer.MinorVersionMatches(version.SemVer("9", "6", "x")) {
 		return f.utilitiesConfig.Postgres96.Client,
 			f.utilitiesConfig.Postgres96.Dump,
 			f.utilitiesConfig.Postgres96.Restore,
