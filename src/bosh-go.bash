9567591773392a055b73d255f20da60df97aa971
Glen Rodgers <grodgers@pivotal.io>
2019-07-05 14:24:17 +0100

    Source golang in bosh-go.bash
    [#167087143]
    
    Signed-off-by: Jake Klein <jklein@pivotal.io>

--- src/bosh-go.bash
+++ src/bosh-go.bash
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+source /var/vcap/packages/golang-1-linux/bosh/compile.env
+
 # This needs to be defined since bosh compilation VMs don't set $HOME.
 export GOCACHE=/tmp/gocache
 
