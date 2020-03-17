2b47d8fa82e0011e1af849483cb965ac94ae7afa
James Norman <jnorman@pivotal.io>
2020-03-17 12:00:07 +0000

    Bump postgres 10 to 10.12 (from 10.9)
    [#171834946]
    
    Signed-off-by: Jake Klein <jklein@pivotal.io>

--- scripts/bump_postgres_blobs.bash
+++ scripts/bump_postgres_blobs.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+BOSH="${BOSH:-bosh}"
+
 # Prior to postgres 10, x.y was a major version (i.e. 9.6).
 # As of postgres 10, they follow semver.
 MAJOR=${MAJOR:?Required: postgres major version}
@@ -15,24 +17,24 @@ old_blob="$(
 )"
 new_version="${MAJOR}.${MINOR}"
 
-sed -i "" \
+sed -i \
     "s/postgresql-.*.tar.gz/postgresql-${new_version}.tar.gz/" \
     "${sdk_root}/packages/database-backup-restorer-postgres-${MAJOR}/spec"
 
-sed -i "" \
+sed -i \
     "s/POSTGRES_VERSION=.*$/POSTGRES_VERSION=${new_version}/" \
     "${sdk_root}/packages/database-backup-restorer-postgres-${MAJOR}/packaging"
 
 wget "--directory-prefix=${sdk_root}" \
   "https://ftp.postgresql.org/pub/source/v${new_version}/postgresql-${new_version}.tar.gz"
 
-bosh remove-blob "--dir=${sdk_root}" "$old_blob"
+"${BOSH}" remove-blob "--dir=${sdk_root}" "$old_blob"
 
-bosh add-blob "--dir=${sdk_root}" \
+"${BOSH}" add-blob "--dir=${sdk_root}" \
   "${sdk_root}/postgresql-${new_version}.tar.gz" \
   "postgres/postgresql-${new_version}.tar.gz"
 
 rm "postgresql-${new_version}.tar.gz"
 
-bosh upload-blobs "--dir=${sdk_root}"
+"${BOSH}" upload-blobs "--dir=${sdk_root}"
 
