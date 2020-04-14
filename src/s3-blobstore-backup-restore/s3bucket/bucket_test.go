1af42d63283a519dcb73a77954996d2bff1a63e6
James Norman <jnorman@pivotal.io>
2020-04-14 15:04:14 +0000

    Test newS3Client at a unit level
    Initially just test the functionality we're adding -- whether we use
    path style or vhost style bucket names.
    
    [#168837909]
    
    Signed-off-by: Gareth Smith <gsmith@pivotal.io>

--- src/s3-blobstore-backup-restore/s3bucket/bucket_test.go
+++ src/s3-blobstore-backup-restore/s3bucket/bucket_test.go
@@ -13,20 +13,20 @@ var _ = Describe("Creating a session on a bucket", func() {
 	When("we want to use a path style bucket addresses", func() {
 		It("adds the appropriate property to the config object", func() {
 
-			session, err := s3bucket.CreateSessionImpl("", nil, "", true)
+			s3Object, err := s3bucket.NewS3ClientImpl("", "", s3bucket.AccessKey{}, false, true)
 
 			Expect(err).NotTo(HaveOccurred())
-			Expect(session.Config.S3ForcePathStyle).To(Equal(aws.Bool(true)))
+			Expect(s3Object.Client.Config.S3ForcePathStyle).To(Equal(aws.Bool(true)))
 		})
 	})
 
 	When("we want to use a v-host style bucket addresses", func(){
 		It("adds the appropriate property to the config object", func() {
 
-			session, err := s3bucket.CreateSessionImpl("", nil, "", false)
+			s3Object, err := s3bucket.NewS3ClientImpl("", "", s3bucket.AccessKey{}, false, false)
 
 			Expect(err).NotTo(HaveOccurred())
-			Expect(session.Config.S3ForcePathStyle).To(Equal(aws.Bool(false)))
+			Expect(s3Object.Client.Config.S3ForcePathStyle).To(Equal(aws.Bool(false)))
 		})
 	})
 })
