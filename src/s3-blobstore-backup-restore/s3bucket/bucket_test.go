06aabdb651958096ee030c477725397d2b9a3ce2
James Norman <jnorman@pivotal.io>
2020-04-15 10:29:20 +0000

    Add unit test for creating an S3 Client
    [#168837909]
    
    Signed-off-by: Gareth Smith <gsmith@pivotal.io>

--- src/s3-blobstore-backup-restore/s3bucket/bucket_test.go
+++ src/s3-blobstore-backup-restore/s3bucket/bucket_test.go
@@ -4,11 +4,41 @@ import (
 	"s3-blobstore-backup-restore/s3bucket"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/client"
+	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Creating a session on a bucket", func() {
+var _ = Describe("Creating an S3 Client", func() {
+
+	When("we are not using an IAMProfile", func() {
+		It("provides static credentials", func() {
+
+			s3Object, err := s3bucket.NewS3ClientImpl("", "", s3bucket.AccessKey{"user", "pass"}, false, false)
+
+			Expect(err).NotTo(HaveOccurred())
+			creds, err := s3Object.Client.Config.Credentials.Get()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(creds.ProviderName).To(Equal("StaticProvider"))
+		})
+	})
+
+	When("we are using an IAMProfile", func() {
+		It("provides EC2 Role credentials", func() {
+			roleCredentials := &credentials.Credentials {}
+
+			s3bucket.SetCredIAMProvider(func(c client.ConfigProvider, options ...func(*ec2rolecreds.EC2RoleProvider)) *credentials.Credentials  {
+				return roleCredentials
+			})
+
+			s3Object, err := s3bucket.NewS3ClientImpl("", "", s3bucket.AccessKey{"user", "pass"}, true, false)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(s3Object.Client.Config.Credentials).To(BeIdenticalTo(roleCredentials))
+		})
+	})
 
 	When("we want to use a path style bucket addresses", func() {
 		It("adds the appropriate property to the config object", func() {
