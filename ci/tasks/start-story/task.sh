b3dc7f3087b9772028b80defd5eb3a38a731d6e2
Neil Hickey <nhickey@pivotal.io>
2021-03-25 13:53:55 -0600

    Only start a story if it's status is `unstarted`
    + This will help mitigate stories state being reverted backwards
      accidentally
    
    [#177453853]
    
    Signed-off-by: Neil Hickey <nhickey@pivotal.io>

--- ci/tasks/start-story/task.sh
+++ ci/tasks/start-story/task.sh
@@ -8,6 +8,21 @@ API_TOKEN=${TRACKER_API_TOKEN}
 PROJECT_ID=${TRACKER_PROJECT_ID}
 STORY_ID=$(cat ./tracker-story/id)
 
+current_state=$(
+    curl \
+        -X GET \
+        -H "X-TrackerToken: $API_TOKEN" \
+        -H "Content-Type: application/json" \
+        "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/stories/$STORY_ID" \
+        -s \
+        | jq '.current_state'
+    )
+
+if [[ "$current_state" != "unstarted" ]]; then
+    echo "Story with ID: $STORY_ID has already been started, refusing to reset it's state to 'started'"
+    exit 0
+fi
+
 err=$(
     curl \
         -X PUT \
@@ -25,4 +40,4 @@ then
     exit 1
 fi
 
-echo "Started story with id $STORY_ID"
+echo "Started story with ID: $STORY_ID"
