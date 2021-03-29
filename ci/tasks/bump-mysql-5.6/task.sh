85b7e4d7f8d2f935fb85ed5a65f7b096c8269b99
Guillermo Bandres <gbandres@vmware.com>
2021-03-29 11:16:44 +0200

    WIP mysql bump task
    [#177478741]

--- ci/tasks/bump-mysql-5.6/task.sh
+++ ci/tasks/bump-mysql-5.6/task.sh
@@ -11,8 +11,7 @@ blobstore:
     secret_access_key: $AWS_SECRET_ACCESS_KEY
 " > config/private.yml
 
-  current_blob_name=$(grep mysql-5.6 config/blobs.yml)
-  current_blob_name=${blob_name%:}
+  current_blob_name=$(grep mysql-5.6 config/blobs.yml | tr -d :)
 
   bosh remove-blob $current_blob_name
 
@@ -20,35 +19,40 @@ blobstore:
 
   echo $new_blob_name
 
-# bosh add-blob /Users/gbandres/Downloads/mysql-5.6.51.tar.gz mysql/mysql-5.6.51.tar.gz
-# bosh upload-blobs
+  bosh add-blob ../mysql-5.6-release/$new_blob_name mysql/$new_blob_name
+  bosh upload-blobs
 
-  # bosh vendor-package "${VENDORED_PACKAGE_NAME}" ../vendored-package-release
+  sed -i "s@$current_blob_name@mysql/$new_blob_name@" packages/database-backup-restorer-mysql-5.6/spec
 
-  # git add .
+  current_mysql_version=$(echo $current_blob_name | cut -c13-18)
+  new_mysql_version=$(echo $new_blob_name | cut -c7-12)
+
+  sed -i "s@$current_mysql_version@$new_mysql_version@" packages/database-backup-restorer-mysql-5.6/packaging
+
+  git add .
   
-  # if [ -z "$VENDOR_UPDATES_BRANCH" ]
-  # then
-  #       curr_branch=$(git rev-parse --abbrev-ref HEAD)
-  #       echo "Pushing package updates to the same branch '${curr_branch}'"
-  # else
-  #       git checkout -b "${VENDOR_UPDATES_BRANCH}"
-  #       echo "Pushing package updates to the configured branch '${VENDOR_UPDATES_BRANCH}'"
-  # fi
+  if [ -z "$VENDOR_UPDATES_BRANCH" ]
+  then
+        curr_branch=$(git rev-parse --abbrev-ref HEAD)
+        echo "Pushing package updates to the same branch '${curr_branch}'"
+  else
+        git checkout -b "${VENDOR_UPDATES_BRANCH}"
+        echo "Pushing package updates to the configured branch '${VENDOR_UPDATES_BRANCH}'"
+  fi
 
-  # if [ -z "${COMMIT_USERNAME}" ] || [ -z "${COMMIT_USEREMAIL}" ]
-  # then
-  #       echo "Unspecified user.name or user.email. Using defaults."
-  # else
-  #       git config user.name "${COMMIT_USERNAME}"
-  #       git config user.email "${COMMIT_USEREMAIL}"
-  # fi
+  if [ -z "${COMMIT_USERNAME}" ] || [ -z "${COMMIT_USEREMAIL}" ]
+  then
+        echo "Unspecified user.name or user.email. Using defaults."
+  else
+        git config user.name "${COMMIT_USERNAME}"
+        git config user.email "${COMMIT_USEREMAIL}"
+  fi
 
-  # if git commit -m "Update package ${VENDORED_PACKAGE_NAME}"; then
-  #   echo "Updated package ${VENDORED_PACKAGE_NAME}"
-  # else
-  #   echo "No change to vendored package ${VENDORED_PACKAGE_NAME}"
-  # fi
+  if git commit -m "Update blob mysql-5.6 from $current_mysql_version to $new_mysql_version"; then
+    echo "Updated blob mysql-5.6"
+  else
+    echo "No change to blob mysql-5.6"
+  fi
 popd
 
-# cp -r release/. release-with-updated-vendored-package
+cp -r release/. release-with-updated-vendored-package
