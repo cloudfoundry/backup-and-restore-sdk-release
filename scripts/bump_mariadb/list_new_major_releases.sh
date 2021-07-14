339b736e8d258ed7a6d806b3870cfbaa3cf4329f
Fernando Naranjo <fnaranjo@vmware.com>
2021-07-14 13:24:57 +0200

    Adapt job bump-mariadb to use "bump-generic" task
    [#178143824]

--- scripts/bump_mariadb/list_new_major_releases.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
-
-pushd "${SCRIPT_DIR}" > /dev/null
-./list_new_stable_releases.sh
-popd > /dev/null
--- scripts/bump_mysql/list_all_stable_releases.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-HTML="$(curl -s -L https://downloads.mysql.com/archives/community/)"
-VALUES="$(echo "${HTML}" | xmllint --html --xpath "//select[@id='version']/option[@value=text()]/@value" - 2>/dev/null)"
-VERSIONS="$(echo "${VALUES}" | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+[a-zA-Z]?')"
-
-echo "${VERSIONS}" | sort -t "." -k1,1n -k2,2n -k3,3n
