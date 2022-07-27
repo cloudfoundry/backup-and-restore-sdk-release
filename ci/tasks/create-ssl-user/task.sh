bdc1341a4f2a3572c1ef3d28fefab3ebce6a9d90
fnaranjo <fnaranjo@vmware.com>
2022-07-27 14:38:06 +0200

    Remove hardcoded port
    [#182318213]

--- ci/tasks/create-ssl-user/task.sh
+++ ci/tasks/create-ssl-user/task.sh
@@ -21,7 +21,7 @@ set -eu
 echo -e "${BOSH_GW_PRIVATE_KEY}" > "${PWD}/ssh.key"
 chmod 0600 "${PWD}/ssh.key"
 export BOSH_GW_PRIVATE_KEY="${PWD}/ssh.key"
-export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}:22?private-key=${BOSH_GW_PRIVATE_KEY}"
+export BOSH_ALL_PROXY="ssh+socks5://${BOSH_GW_USER}@${BOSH_GW_HOST}?private-key=${BOSH_GW_PRIVATE_KEY}"
 
 bosh \
   --deployment "${BOSH_DEPLOYMENT}" \
