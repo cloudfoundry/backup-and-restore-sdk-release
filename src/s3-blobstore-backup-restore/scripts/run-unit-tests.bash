9db0866ac43279e7d4b77c9559bb8b3d7aa31dd9
Gareth Smith <sgareth@vmware.com>
2022-09-28 11:15:41 -0300

    Refactor S3 tests to use the generic Dockerfile
    [#183164582]
    
    Signed-off-by: Diego Lemos <dlemos@vmware.com>
    Signed-off-by: Fernando Naranjo <fnaranjo@vmware.com>
    Signed-off-by: Iain Findlay <fiain@vmware.com>

---
+++ src/s3-blobstore-backup-restore/scripts/run-unit-tests.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SRC_DIR="$(cd "$( dirname "$0" )/.." && pwd)"
+
+set -x
+pushd "$SRC_DIR"
+  ginkgo -mod vendor -r -keepGoing -p --skipPackage s3bucket
+popd
