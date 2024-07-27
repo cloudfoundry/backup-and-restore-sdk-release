655bc0b53436a516b175d77d425aac8fd0ee8361
Joseph Palermo <joseph.palermo@broadcom.com>
2024-07-26 19:19:45 -0700

    Revert increasing compilation VM size, it did not fix the mariadb problem

--- ci/tasks/update-cloud-config/task.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-set -x
-
-eval "$(bbl print-env --metadata-file cf-deployment-env/metadata)"
-
-bosh cloud-config > cc.yml
-bosh update-cloud-config -n cc.yml -o backup-and-restore-sdk-release/ci/tasks/update-cloud-config/update-compilation-vm.yml
