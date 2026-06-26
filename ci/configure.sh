ae8269d9954945e5850cd44429014072ec4ffafe
aram price <aram.price@broadcom.com>
2026-06-26 11:01:22 -0700

    ci: update configure script for migration

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eu -o pipefail
 
-[ -z "${DEBUG:-}" ] || set -x
+if [[ -n "${DEBUG:-}" ]]; then
+  set -x
+fi
 
-fly -t "${CONCOURSE_TARGET:-bosh-ecosystem}" \
-  set-pipeline -p backup-and-restore-sdk-release \
-  -c ci/pipeline.yml
+REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+concourse_target="${CONCOURSE_TARGET:-bosh}"
+fly="${FLY_CLI:-fly}"
+
+pipeline_name="backup-and-restore-sdk-release"
+pipeline_config="${REPO_ROOT}/ci/pipeline.yml"
+
+echo "Validating..."
+"${fly}" validate-pipeline --config "${pipeline_config}" # TODO: add back '--strict'
+echo ""
+
+"${fly}" -t "${concourse_target}" \
+  set-pipeline \
+    --pipeline "${pipeline_name}" \
+    --config "${pipeline_config}"
