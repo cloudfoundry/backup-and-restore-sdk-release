2cb420fef6936891db62ab7691b8806ffcc6ed67
aram price <aram.price@broadcom.com>
2026-06-26 11:18:53 -0700

    ci: extract IAM vm_extension update into a versioned ops file
    Move the inline cloud-config patch from bbl-up-aws/task.sh into
    ci/manifests/ops-files/iam-instance-profile.yml so the ops file is
    reviewed and tested alongside the manifest that consumes it.

--- ci/tasks/bbl-up-aws/task.sh
+++ ci/tasks/bbl-up-aws/task.sh
@@ -16,16 +16,10 @@ pushd "${PWD}/bbl-state"
   # s3-backuper-with-iam-instance-profile manifest can reference it.
   eval "$(bbl print-env)"
 
-  cat > /tmp/iam-vm-extension-ops.yml << OPSEOF
-- type: replace
-  path: /vm_extensions/-
-  value:
-    name: s3-backup-iam-instance-profile
-    cloud_properties:
-      iam_instance_profile: ${BBL_AWS_IAM_INSTANCE_PROFILE}
-OPSEOF
-
   bosh cloud-config > /tmp/current-cloud-config.yml
-  bosh int /tmp/current-cloud-config.yml -o /tmp/iam-vm-extension-ops.yml > /tmp/updated-cloud-config.yml
+  OPS_FILE="${PWD}/../backup-and-restore-sdk-release-ci/ci/manifests/ops-files/iam-instance-profile.yml"
+  bosh int /tmp/current-cloud-config.yml \
+    -o "${OPS_FILE}" \
+    -v iam_instance_profile="${BBL_AWS_IAM_INSTANCE_PROFILE}" > /tmp/updated-cloud-config.yml
   bosh update-cloud-config /tmp/updated-cloud-config.yml --non-interactive
 popd
